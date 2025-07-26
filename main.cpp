fc42412d7c2f376bb422e024875644ec848c9edc
SadiqParvizoglu <150825954+SadiqParvizoglu@users.noreply.github.com>
2025-07-26 21:49:08 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
-#include <bits/stdc++.h>
-#include <Boota/BMP/BMP.h>
+#include <Boota/Window/BMPWindow.h>
 #include <Boota/V4L2Camera/V4L2Camera.h>
-using namespace std;
+#include <Boota/BMP/BMP.h>
+#include <thread>
+#include <chrono>
+
+int winWidth=1280,winHeight=720;
 
 int main()
 {
-    V4L2Camera camera("/dev/video0", 640, 480);
-    BMP bmp = camera.captureBMP();
-    bmp.write("capture.bmp");
+    BMPWindow window(winWidth,winHeight,"Boota Camera Window");
+    V4L2Camera camera("/dev/video0",winWidth,winHeight);
+    BMP bmp(10,10,false);
+    while (window.isOpen())
+    {
+        window.pollEvents();
+        window.clear();
+
+        bmp=camera.captureBMP();
+        window.showBMP(bmp);
+
+        window.display();
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
     return 0;
 }
